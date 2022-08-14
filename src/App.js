8de455693638b1b6fabc304f56b43b87437cda51
splash <splashs089@gmail.com>
2022-08-14 17:44:30 +0100

    adding bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import { Form } from "./components/Form";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="container">
+      <Form />
+    </div>
+  );
 }
 
 export default App;
